4e7251cae208261ee75c66f43c84562041a88f16
yoavabadi2018 <38031550+yoavabadi2018@users.noreply.github.com>
2018-06-26 17:50:55 +0300

    Add files via upload

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -19,31 +19,81 @@ pagesAddresses[TABLES_DEPTH]);
 
 
 
-// DFS recursive implementation
-word_t findEmptyFrame()
+void dfs();
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+void createAddressTable(uint64_t fullVirtualAddress, uint64_t
+pagesAddresses[TABLES_DEPTH])
 {
-    uint64_t root = 0;
-
-
+    for (int i = 0; i < TABLES_DEPTH ; i++)
+    {
+        pagesAddresses[(TABLES_DEPTH - 1) - i] =
+                (fullVirtualAddress >> OFFSET_WIDTH * i )
+                % (2 << (OFFSET_WIDTH - 1));
+    }
 }
 
 
 
-
-void dfs(uint64_t physicalAddress, word_t value)
+void dfs(uint64_t currentFrame)
 {
-    PMread(physicalAddress, &value);
-    if(value != 0)
+    word_t value;
+
+    for(int i = 0; i < PAGE_SIZE; i++)
     {
-        for(int i = 0; i < PAGE_SIZE; i++)
-        {
+        PMread(currentFrame * PAGE_SIZE + i, &value);
 
 
 
-            dfs(physicalAddress + i, value);
 
-        }
+
     }
+
+
+
+}
+
+// DFS recursive implementation
+uint64_t findEmptyFrame(uint64_t fullVirtualAddress)
+{
+    uint64_t root = 0;
+    uint64_t maxFrameVisited = 0;
+    uint64_t maxDistance = 0;
+    uint64_t targetFrame = 0;
+    uint64_t targetFrameParent = 0;
+    uint64_t maxDistanceFrame = 0; // TODO: Understand why to use distance
+    uint64_t distanceParent = 0; // TODO: As well
+    uint64_t maxDistanceVirtualAddress = 0; // TODO: As well
+    uint64_t targetVirtualAddress = 0;
+
+//    dfs(); // TODO: Put all the fucking values
+
+    if ((maxFrameVisited + 1) < NUM_FRAMES)
+    {
+        return maxFrameVisited + 1;
+    }
+
+    else if (targetFrame > 0 && (maxFrameVisited + 1) == NUM_FRAMES)
+    {
+        // TODO: func - helper - getOffset
+        uint64_t offset = targetVirtualAddress % (2 << (OFFSET_WIDTH - 1));
+        // TODO: split to func - write to parent
+        PMwrite(targetFrameParent * PAGE_SIZE + offset, 0);
+        return targetFrame;
+    }
+    else
+    {
+        PMevict(maxDistanceFrame, maxDistanceVirtualAddress);
+        clearTable(maxDistanceFrame);
+        uint64_t offset = maxDistanceVirtualAddress % (2 << (OFFSET_WIDTH - 1));
+        PMwrite(distanceParent * PAGE_SIZE + offset, 0);
+        return maxDistanceFrame;
+    }
+
 }
 
 
@@ -73,54 +123,51 @@ int VMwrite(uint64_t virtualAddress, word_t value)
          " address : " << fullVirtualAddress << endl;
 
 
+
     uint64_t rootFrame = 0;
 
-    // Create address table
     uint64_t pagesAddresses[TABLES_DEPTH] = {0};
 
-    for (int i = 0; i < TABLES_DEPTH ; i++)
-    {
-        pagesAddresses[(TABLES_DEPTH - 1) - i] =
-                        (fullVirtualAddress >> OFFSET_WIDTH * i )
-                                        % (2 << (OFFSET_WIDTH - 1));
-    }
+    createAddressTable(fullVirtualAddress, pagesAddresses);
 
 
 
+    word_t addr1 = rootFrame;
+    word_t addr2;
 
-
-    word_t addr = 0;
     for (int j = 0; j < TABLES_DEPTH; j++)
     {
-        word_t tmp_addr = addr;
-        PMread((addr * PAGE_SIZE) + pagesAddresses[j], &addr);
+        PMread((addr1 * PAGE_SIZE) + pagesAddresses[j], &addr2);
 
-        if (addr == 0)
+        if (addr2 == 0)
         {
             // findEmptyFrame --> dfs + clearTable;
-            word_t frameIndex = findEmptyFrame();
-            PMwrite((tmp_addr * PAGE_SIZE) + pagesAddresses[j], frameIndex);
-            tmp_addr = addr;
-            addr = frameIndex;
+            word_t frameIndex = findEmptyFrame(fullVirtualAddress); // f1 - frameIndex1
+
+            if(j+1 < TABLES_DEPTH)
+            {
+                clearTable(frameIndex);
+            }
+            else
+            {
+                PMrestore(frameIndex, fullVirtualAddress);
+            }
+
+            PMwrite((addr1 * PAGE_SIZE) + pagesAddresses[j], frameIndex);
+            addr2 = frameIndex;
         }
-
-        cout << "addr : " << (int)addr << endl;
+        addr1 = addr2;
+        cout << "addr1 : " << (int)addr1 << endl;
     }
 
-
-
-
-    for (int k = 0; k < TABLES_DEPTH; ++k)
+    if(addr2 < 0)
     {
-        cout << k << " : " << pagesAddresses[k] << endl;
+        return 0; // fail
     }
+    PMwrite(addr2 * PAGE_SIZE + offset, value);
+    return 1;
 
-
-
-
-
-
-
+    // address held at addr2
 }
 
 
